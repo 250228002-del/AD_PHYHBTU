af72cce608daf3197e4a8e042d2d8f3797034ddc
250228002-del <250228002@hbtu.ac.in>
2026-08-14 14:21:52 +0530

    Update README.py

--- README.py
+++ README.py
@@ -29,7 +29,7 @@ st.markdown("""
         border: 2px solid #dce5ff;
         border-radius: 10px;
         padding: 5px;
-        margin: 15px 0;
+        margin: 10px 0;
         background: #ffffff;
     }
 
