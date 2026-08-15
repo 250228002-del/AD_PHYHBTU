0390bc4dde08e92c771ceb2ccdf0e4cdb5cded13
250228002-del <250228002@hbtu.ac.in>
2026-08-15 21:48:47 +0530

    Update README.py

--- README.py
+++ README.py
@@ -422,9 +422,9 @@ def convert_magnetic_moment(value, unit):
 import math
 def convert_magnetic_moment_per_unit_mass(value, unit):
     if unit in ["A m² kg⁻¹", "A m² kg⁻¹", "A m^2/kg", "A m² kg-1"]:
-        return value * 10**3, "emu g⁻¹"   
+        return value * 1, "emu g⁻¹"   
     elif unit in ["emu g⁻¹", "EMU"]:
-        return value / 10**3, "A m² kg⁻¹" 
+        return value * 1, "A m² kg⁻¹" 
     else:
         return value, unit
 
