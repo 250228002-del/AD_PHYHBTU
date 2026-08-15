69588cbc70bed436bf3ab0a2978ea16585cbf635
250228002-del <250228002@hbtu.ac.in>
2026-08-15 20:06:18 +0530

    Update README.py

--- README.py
+++ README.py
@@ -381,7 +381,7 @@ def convert_magnetic_field(value, unit):
     if unit in ["Oersted (Oe)", "Oe", "oersted(Oe)", "oersted"]:
         return value / (4 * math.pi * 10**-3), "A m^-1"   
     elif unit in ["A m^-1", "A/m", "A m−1", "Ampere per meter"]:
-        return value * (4 * math.pi * 10**-3), "oersted(Oe)" 
+        return value * (4 * math.pi * 10**-3), "oersted(Oe)"
     else:
         return value, unit
 
