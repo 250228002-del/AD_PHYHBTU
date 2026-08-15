556ae4f4c756ce5d568c16b39885a51ad6d4ad9b
250228002-del <250228002@hbtu.ac.in>
2026-08-15 14:45:20 +0530

    Update README.py

--- README.py
+++ README.py
@@ -641,10 +641,10 @@ elif quantity == "Energy product((BH)max)":
 # 4. Answer
 # -----------------------------
 st.markdown(
-    '<div class="ansewr"><div>
-    class="answer-title">4. Your answer</div>',
+    '<div class="ansewr"><div
+class="answer-title">4. Your answer</div>',
     unsafe_allow_html=True
- )
+)
  
 st.markdown(
     f"""
